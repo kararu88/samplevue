b4d191c58819d09a1cab0a00d0e0a25c1e37467c
karasawa <satoru.karasawa@nifty.com>
2019-10-16 18:11:54 +0900

    First Commit.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <FirstVueClass msg1="Hello My First Vue Class" msg2="OMG!!"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import FirstVueClass from './components/FirstVueClass.vue'
+
+export default {
+  name: 'app',
+  components: {
+    FirstVueClass
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FirstVueClass.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="firstvueclass">
+        <p>日本語も取り扱えちゃったりしちゃう系よ！</p>
+        <p>args1 : {{ msg1 }}</p>
+        <p>args1 : {{ msg2 }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FirstVueClass",
+        props: {
+            msg1: String,
+            msg2: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
