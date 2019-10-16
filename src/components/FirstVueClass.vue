d76fbf0953bc4e690afb5dc2323e25cb0133e616
karasawa <satoru.karasawa@nifty.com>
2019-10-16 23:47:28 +0900

    Study vue component system.

--- src/components/FirstVueClass.vue
+++ src/components/FirstVueClass.vue
@@ -1,8 +1,14 @@
 <template>
+    <!-- Vue.jsでは、この書き方を単一ファイルコンポーネントというみたい -->
     <div class="firstvueclass">
         <p>日本語も取り扱えちゃったりしちゃう系よ！</p>
         <p>args1 : {{ msg1 }}</p>
         <p>args1 : {{ msg2 }}</p>
+        <ul>
+            <li v-for="product in products">
+                {{ product }}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -12,6 +18,18 @@
         props: {
             msg1: String,
             msg2: String
+        },
+        data () {
+            return {
+                products: []
+            }
+        },
+        created () {
+            fetch('https://api.myjson.com/bins/74l63')
+                .then(response => response.json())
+                .then(json => {
+                    this.products = json.products
+                })
         }
     }
 </script>
